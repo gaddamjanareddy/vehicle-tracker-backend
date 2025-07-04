f7b5fd6e8206deafef3a1273b13abd8cd35a62c5
gaddamjanareddy <153525326+gaddamjanareddy@users.noreply.github.com>
2025-07-04 18:15:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/api/route", (req, res) => {
   res.json(routeData);
 });
 
-const PORT = 5000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
